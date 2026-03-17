594508158fe1e8bfb75a2ffbc406a0426c609d5e
Sylvain Maltais <job@gladir.com>
2026-03-17 15:32:55 -0400

    Correctif

--- SAMPLES/QUICKPASCAL/test_msgraph_draw.pas
+++ SAMPLES/QUICKPASCAL/test_msgraph_draw.pas
@@ -1,7 +1,11 @@
 { test_msgraph_draw.pas - Test MSGraph drawing procedures (QuickPascal mode) }
 program test_msgraph_draw;
-uses MSGraph;
+uses Crt,MSGraph;
 begin
+ { Test _SetVideoMode (stub) }
+  WriteLn('_SetVideoMode=', _SetVideoMode(_VRes16Color));
+
+
   { Test _MoveTo }
   _MoveTo(100, 200);
   WriteLn('_MoveTo(100,200) done');
@@ -30,8 +34,7 @@ begin
   _FloodFill(100, 100, 15);
   WriteLn('_FloodFill done');
 
-  { Test _SetVideoMode (stub) }
-  WriteLn('_SetVideoMode=', _SetVideoMode(_VRes16Color));
-
+ 
   WriteLn('OK');
+  Readkey;
 end.
